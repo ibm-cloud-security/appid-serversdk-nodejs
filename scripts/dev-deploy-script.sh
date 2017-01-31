f0ce47a91e5a07ea3874207e9ff5438f7bd01917
Anton Aleksandrov <antona@us.ibm.com>
2017-01-31 16:16:23 -0600

    test

--- scripts/dev-deploy-script.sh
+++ scripts/dev-deploy-script.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 set -ev
-# if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-# 	bundle exec rake test:integration
-# fi
+echo TRAVIS_PULL_REQUEST $TRAVIS_PULL_REQUEST
+echo TRAVIS_BRANCH $TRAVIS_BRANCH
+echo TRAVIS_NODE_VERSION $TRAVIS_NODE_VERSION
+
+if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
+	exit 0
+fi
+if [ "${TRAVIS_NODE_VERSION}" != "false" ]; then
+	exit 0
+fi
+
 echo "hello!"
-# $TRAVIS_NODE_VERSION
-# TRAVIS_BRANCH
-# TRAVIS_PULL_REQUEST == # or "false"
