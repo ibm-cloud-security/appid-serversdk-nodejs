72797b30319a2af099af6f810de2a91106822fcb
Anton Aleksandrov <antona@us.ibm.com>
2017-02-14 17:36:24 -0600

    Added copyright headers

--- samples/web-app-sample.js
+++ samples/web-app-sample.js
@@ -9,6 +9,19 @@ const WebAppStrategy = require("./../lib/appid-sdk").WebAppStrategy;
 const app = express();
 
 // Below URLs will be used for AppID OAuth flows
+/*
+ Copyright 2017 IBM Corp.
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
 const LOGIN_URL = "/ibm/bluemix/appid/login";
 const CALLBACK_URL = "/ibm/bluemix/appid/callback";
 const LOGOUT_URL = "/ibm/bluemix/appid/logout";
