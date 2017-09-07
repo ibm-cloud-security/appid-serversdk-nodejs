977f724e29adcdb0268f62be13465df67c925f56
rotembr <rotembr@il.ibm.com>
2017-09-07 10:03:22 +0300

    finish test + fix

--- test/public-key-util-test.js
+++ test/public-key-util-test.js
@@ -14,103 +14,178 @@
 const chai = require("chai");
 const assert = chai.assert;
 const proxyquire = require("proxyquire");
-const testServerUrl = "https://mobileclientaccess.ng.bluemix.net/imf-authserver";
+const testServerUrl = "https://mobileclientaccess.test.url/imf-authserver";
+var requestCounter = 0;
+var seqRequestCounter = 0;
+const Q = require("q");
 
-describe("/lib/utils/public-key-util", function(){
+describe("/lib/utils/public-key-util", function () {
 	console.log("Loading public-key-util-test.js");
-
+	
 	var PublicKeyUtil;
-
-	before(function(){
+	
+	before(function () {
 		PublicKeyUtil = proxyquire("../lib/utils/public-key-util", {
 			"request": requestMock
 		});
 	});
-
+	
 	this.timeout(5000);
-
-	describe('#retrievePublicKeys()', function(){
-		it("Should fail retrieving public key from the server", function(done){
+	
+	describe("setPublicKeysEndpoint", function () {
+		it("Should set server public key endpoint", function (done) {
 			PublicKeyUtil.setPublicKeysEndpoint(testServerUrl);
 			try {
 				assert.equal(testServerUrl + "/publickeys", PublicKeyUtil.getPublicKeysEndpoint());
 				done();
-			} catch(e) {
+			} catch (e) {
 				done(e);
 			}
 		});
 	});
-
-	describe("#getPublicKeyPem()", function(){
-		it("Should fail to get previously retrieved public key", function(){
-			PublicKeyUtil.getPublicKeyPemByKid().then(function (publicKey) {
-				assert.isUndefined(publicKey);
-			});
-		});
-	});
-
-	describe('#retrievePublicKeys()', function(){
-		it("Should successfully retrieve public key from OAuth server", function(done){
-			PublicKeyUtil.setPublicKeysEndpoint(testServerUrl + "SUCCESS-PUBLIC-KEYs");
-			PublicKeyUtil.getPublicKeyPemByKid("123").then(function(){
+	
+	describe("getPublicKeyPemByKid", function () {
+		
+		it("public key dont have kid value", function (done) {
+			var kid = undefined;
+			PublicKeyUtil.getPublicKeyPemByKid(kid).then(function (publicKey) {
+				done("should get to catch");
+			}).catch(function (err) {
+				assert.equal(err, "Passed token does not have kid value.");
 				done();
-			}).catch(function(err){
-				done(new Error(err));
+			})
+		});
+		
+		it("request to public keys endpoint failure", function (done) {
+			PublicKeyUtil.setPublicKeysEndpoint(testServerUrl + "FAIL-PUBLIC-KEYs");
+			var kid = "not_found_kid";
+			PublicKeyUtil.getPublicKeyPemByKid(kid).then(function (publicKey) {
+				done("should get reject");
+			}).catch(function (err) {
+				try {
+					assert.equal(err, "updatePublicKeys error: Failed to retrieve public keys.  All requests to protected endpoints will be rejected.");
+					done();
+				} catch(e) {
+					done(e);
+				}
+			});
+		});
+		
+		it("request to public keys endpoint update failure", function (done) {
+			PublicKeyUtil.setPublicKeysEndpoint(testServerUrl + "SUCCESS-PUBLIC-KEYs");
+			var kid = "123";
+			PublicKeyUtil.getPublicKeyPemByKid(kid).then(function (publicKey) {
+				PublicKeyUtil.setPublicKeysEndpoint(testServerUrl + "FAIL-PUBLIC-KEYs");
+				kid = "not_found_kid";
+				PublicKeyUtil.getPublicKeyPemByKid(kid).then(function (publicKey) {
+					done("should get reject");
+				}).catch(function (err) {
+					try {
+						assert.equal(err, "updatePublicKeys error: Failed to update public keys.");
+						done();
+					} catch(e) {
+						done(e);
+					}
+				});
+			}).catch(function (err) {
+				done(e);
+			});
+		});
+		
+		it("two sequential request to public keys endpoint", function (done) {
+			var PublicKeyUtilNew = proxyquire("../lib/utils/public-key-util", {
+				"request": requestMock
+			});
+			PublicKeyUtilNew.setPublicKeysEndpoint(testServerUrl + "SEQUENTIAL-REQUEST-PUBLIC-KEYs");
+			var kid = "123";
+			PublicKeyUtilNew.getPublicKeyPemByKid(kid).then(function (publicKey) {
+				PublicKeyUtilNew.getPublicKeyPemByKid(kid).then(function (publicKey) {
+					assert.equal(1, seqRequestCounter, "more then one request triggered");
+					done();
+				}).catch(function (err) {
+					done(err);
+				});
+			}).catch(function (err) {
+				done(err);
+			});
+		});
+		
+		it("Should successfully retrieve public key from OAuth server", function (done) {
+			PublicKeyUtil.setPublicKeysEndpoint(testServerUrl + "SUCCESS-PUBLIC-KEYs");
+			var kid = "123";
+			PublicKeyUtil.getPublicKeyPemByKid(kid).then(function (publicKey) {
+				try {
+					assert.isNotNull(publicKey);
+					assert.isString(publicKey);
+					assert.include(publicKey, "BEGIN RSA PUBLIC KEY");
+					done();
+				} catch (e) {
+					done(e);
+				}
+			}).catch(function (err) {
+				done(err);
 			});
 		});
 	});
-
-	describe("#getPublicKeyPem()", function(){
-		it("Should get previously retrieved public key", function(){
-			PublicKeyUtil.getPublicKeyPemByKid("123").then(function (publicKey){
-				assert.isNotNull(publicKey);
-				assert.isString(publicKey);
-				assert.include(publicKey, "BEGIN RSA PUBLIC KEY");
+	
+	describe("getPublicKeyPemMultipleRequests", function () {
+		it("Should get public keys from multiple requests", function (done) {
+			var PublicKeyUtilNew = proxyquire("../lib/utils/public-key-util", {
+				"request": requestMock
 			});
+			PublicKeyUtilNew.setPublicKeysEndpoint(testServerUrl + "SETTIMEOUT-PUBLIC-KEYs");
+			var requestArray = [];
+			for (var i = 0; i < 5; i++) {
+				requestArray.push(PublicKeyUtilNew.getPublicKeyPemByKid("123"));
+			}
+			Q.all(requestArray).then(function (publicKeysArray) {
+				try {
+					assert.equal(1, requestCounter, "more then one request triggered");
+					for (var i = 0; i < 5; i++) {
+						assert.isNotNull(publicKeysArray[i]);
+						assert.isString(publicKeysArray[i]);
+						assert.include(publicKeysArray[i], "BEGIN RSA PUBLIC KEY");
+					}
+					done();
+				} catch (e) {
+					done(e);
+				}
+			}).catch(function (err) {
+				done(err);
+			});
+			
+			
 		});
 	});
-
-    describe("#getPublicKeyPemMultipleRequests()", function(){
-        it("Should get public keys from multiple requests and empty the requests cache", function(){
-	        PublicKeyUtilNew = proxyquire("../lib/utils/public-key-util", {
-		        "request": requestMock
-	        });
-	        PublicKeyUtilNew.setPublicKeysEndpoint(testServerUrl + "SETTIMEOUT-PUBLIC-KEYs");
-	        PublicKeyUtilNew.getPublicKeyPemByKid("123").then(function(){});
-            for (var i=0;i<5;i++) {
-	            PublicKeyUtilNew.getPublicKeyPemByKid("123").then(function (publicKey) {
-                    assert.isNotNull(publicKey);
-                    assert.isString(publicKey);
-                    assert.include(publicKey, "BEGIN RSA PUBLIC KEY");
-                });
-            }
-        });
-    });
 });
 
-var requestMock = function (options, callback){
-	if (options.url.indexOf("FAIL-PUBLIC-KEY") >=0  || options.url.indexOf("FAIL_REQUEST") >= 0){ // Used in public-key-util-test
+var requestMock = function (options, callback) {
+	if (options.url.indexOf("FAIL-PUBLIC-KEY") >= 0 || options.url.indexOf("FAIL_REQUEST") >= 0) { // Used in public-key-util-test
 		return callback(new Error("STUBBED_ERROR"), {statusCode: 0}, null);
-	} else if (options.url.indexOf("SUCCESS-PUBLIC-KEY") !== -1){ // Used in public-key-util-test
-		return callback(null, { statusCode: 200}, {"keys": [{"n":"1", "e":"2", "kid":"123"}]});
-	} else if (options.formData && options.formData.code && options.formData.code.indexOf("FAILING_CODE") !== -1){ // Used in webapp-strategy-test
+	} else if (options.url.indexOf("SUCCESS-PUBLIC-KEY") !== -1) { // Used in public-key-util-test
+		return callback(null, {statusCode: 200}, {"keys": [{"n": "1", "e": "2", "kid": "123"}]});
+	} else if (options.formData && options.formData.code && options.formData.code.indexOf("FAILING_CODE") !== -1) { // Used in webapp-strategy-test
 		return callback(new Error("STUBBED_ERROR"), {statusCode: 0}, null);
-	} else if (options.formData && options.formData.code && options.formData.code.indexOf("WORKING_CODE") !== -1){ // Used in webapp-strategy-test
+	} else if (options.formData && options.formData.code && options.formData.code.indexOf("WORKING_CODE") !== -1) { // Used in webapp-strategy-test
 		return callback(null, {statusCode: 200}, JSON.stringify({
 			"access_token": "access_token_mock",
 			"id_token": "id_token_mock"
 		}));
-	} else if (options.followRedirect === false){
+	} else if (options.followRedirect === false) {
 		return callback(null, {
 			statusCode: 302,
 			headers: {
 				location: "test-location?code=WORKING_CODE"
 			}
 		});
-	} else if (options.url.indexOf("SETTIMEOUT-PUBLIC-KEYs") > -1){
-		setTimeout(function() {
-            return callback(null, { statusCode: 200}, {"keys": [{"n":"1", "e":"2", "kid":"123"}]});
+	} else if (options.url.indexOf("SETTIMEOUT-PUBLIC-KEYs") > -1) {
+		requestCounter++;
+		setTimeout(function () {
+			return callback(null, {statusCode: 200}, {"keys": [{"n": "1", "e": "2", "kid": "123"}]});
 		}, 3000);
+	} else if(options.url.indexOf("SEQUENTIAL-REQUEST-PUBLIC-KEYs") > -1) {
+		seqRequestCounter++;
+		return callback(null, {statusCode: 200}, {"keys": [{"n": "1", "e": "2", "kid": "123"}]});
 	} else {
 		throw "Unhandled case!!!" + JSON.stringify(options);
 	}
