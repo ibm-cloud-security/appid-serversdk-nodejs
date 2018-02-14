85c5f3d4385ce09176095201cb0984044769f384
rotembr <rotembr@il.ibm.com>
2018-02-14 21:15:21 +0200

    added forgot password to CD b-y-o example

--- samples/cloud-directory-app-sample-server.js
+++ samples/cloud-directory-app-sample-server.js
@@ -21,6 +21,8 @@ const bodyParser = require("body-parser"); // get information from html forms
 const flash = require("connect-flash");
 const app = express();
 const logger = log4js.getLogger("cloud-directory-app-sample-server");
+const base64url = require('base64url');
+const crypto = require('crypto');
 
 const LANDING_PAGE_URL = "/cloud-directory-app-sample.html";
 const CALLBACK_URL = "/ibm/bluemix/appid/callback";
@@ -31,12 +33,15 @@ const PROTECTED_ENDPOINT = "/protected";
 const SIGN_UP_PAGE = "/ibm/bluemix/appid/view/sign_up";
 const FORGOT_PASSWORD_PAGE = "/ibm/bluemix/appid/view/forgot_password";
 const ACCOUNT_CONFIRMED_PAGE = "/ibm/bluemix/appid/view/account_confirmed";
+const RESET_PASSWORD_PAGE = "/ibm/bluemix/appid/view/reset_password_form";
 const SIGN_UP_SUBMIT = "/sign_up/submit";
 const SIGN_UP_SUBMIT_MOBILE = "/sign_up/mobile/submit";
 const FORGOT_PASSWORD_SUBMIT = "/forgot_password/submit";
 const FORGOT_PASSWORD_SUBMIT_MOBILE = "/forgot_password/mobile/submit";
 const RESEND_NOTIFICATION = "/resend_notification";
 const GENERAL_ERROR = "GENERAL_ERROR";
+const USER_NOT_FOUND = "userNotFound";
+const RESET_PASSWORD_SUBMIT = "/reset_password/submit";
 
 const loginEjs = 'cd_login.ejs';
 const selfSignUpEjs = 'self_sign_up.ejs';
@@ -44,6 +49,11 @@ const selfForgotPasswordEjs = 'self_forgot_password.ejs';
 const resetPasswordSentEjs = 'reset_password_sent.ejs';
 const thanksForSignUpEjs = 'thanks_for_sign_up.ejs';
 const accountConfirmedEjs = 'account_confirmed.ejs';
+const resetPasswordFormEjs = 'reset_password_form.ejs';
+const resetPasswordExpiredEjs = 'reset_password_expired.ejs';
+const resetPasswordSuccess = 'reset_password_success.ejs';
+
+let resetPasswordCodesMap = new Map();
 
 app.use(helmet());
 app.use(flash());
@@ -66,20 +76,22 @@ app.use(express.static(__dirname ));
 app.use(passport.initialize());
 app.use(passport.session());
 
+const tenantId = "379c9bd2-8d02-4b5b-83d3-24ad9440a0e3";
+
 // Configure passportjs to use WebAppStrategy
 passport.use(new WebAppStrategy({
-	tenantId: "379c9bd2-8d02-4b5b-83d3-24ad9440a0e3",
+	tenantId: tenantId,
 	clientId: "dfbd817a-f12e-4d06-9e98-a8863085856b",
 	secret: "NTgyMzE5YjctNTM3OS00YWE0LWIzYTItYTc0N2MwNzkxODU1",
-	oauthServerUrl: "https://appid-oauth.stage1.eu-gb.bluemix.net/oauth/v3/379c9bd2-8d02-4b5b-83d3-24ad9440a0e3",
+	oauthServerUrl: "http://localhost:6002/oauth/v3/379c9bd2-8d02-4b5b-83d3-24ad9440a0e3",//"https://appid-oauth.stage1.eu-gb.bluemix.net/oauth/v3/379c9bd2-8d02-4b5b-83d3-24ad9440a0e3",
 	redirectUri: "http://localhost:1234" + CALLBACK_URL
 }));
 
 const selfServiceManager = require("./../lib/appid-sdk").selfServiceManager;
 selfServiceManager.init({
 	iamApiKey: "vtPMY4LXkZWX1FCjRKf7qb1f-yh8y-jDLhTWVMVXJNmA",
-	tenantId: "379c9bd2-8d02-4b5b-83d3-24ad9440a0e3",
-	managementUrl: "https://appid-management.stage1.eu-gb.mybluemix.net/management/v4/379c9bd2-8d02-4b5b-83d3-24ad9440a0e3"
+	tenantId: tenantId,
+	managementUrl: "http://localhost:8080/management/v4/379c9bd2-8d02-4b5b-83d3-24ad9440a0e3"//"https://appid-management.stage1.eu-gb.mybluemix.net/management/v4/379c9bd2-8d02-4b5b-83d3-24ad9440a0e3"
 });
 
 // Configure passportjs with user serialization/deserialization. This is required
@@ -180,8 +192,9 @@ function _render(res, ejs, inputs, language = 'en', errorCode) {
 			message: errorMsg
 		};
 		Object.assign(languageStrings, previousInputs);
-	} else if(ejs === loginEjs) {
+	} else if(ejs === loginEjs || ejs === selfForgotPasswordEjs || ejs === resetPasswordFormEjs) {
 		Object.assign(languageStrings, {message: errorMsg});
+		Object.assign(languageStrings, inputs);
 	} else {
 		Object.assign(languageStrings, inputs);
 	}
@@ -221,7 +234,7 @@ app.post(SIGN_UP_SUBMIT, bodyParser.urlencoded({extended: false}), function(req,
 	}).catch(function (err) {
 		if (err.code) {
 			logger.debug("error code:" + err.code + " ,bad sign up input: " + err.message);
-			_render(res, selfSignUpEjs, req.body, req.query.language, err.code);
+			_render(res, selfSignUpEjs, req.body, language, err.code);
 		} else {
 			logger.error(err);
 			res.status(500);
@@ -243,8 +256,8 @@ app.post(FORGOT_PASSWORD_SUBMIT_MOBILE, bodyParser.json(), function(req, res) {
 		logger.debug('forgot password success');
 		res.status(202).send(user);
 	}).catch(function (err) {
-		res.status(err && err.code || 500);
-		if (err && err.code >= 400 && err.code < 500) {
+		res.status(err && err.statusCode || 500);
+		if (err && err.statusCode >= 400 && err.statusCode < 500) {
 			logger.debug("bad input for forgot password: " + err.message);
 			res.send(err.message);
 		} else {
@@ -259,15 +272,15 @@ app.post(FORGOT_PASSWORD_SUBMIT, bodyParser.urlencoded({extended: false}), funct
 	let email = req.body && req.body.email;
 	let language = req.query.language;
 	selfServiceManager.forgotPassword(email, language).then(function (user) {
-		res.render(resetPasswordSentEjs,  {
+		_render(res, resetPasswordSentEjs,  {
 			displayName: user.displayName ,
 			email: user.emails[0].value,
 			uuid: user.id
-		});
+		}, language);
 	}).catch(function (err) {
-		if (err && err.code >= 400 && err.code < 500) {
+		if (err && err.statusCode === 404) {
 			logger.debug("bad input for forgot password: " + err.message);
-			_render(res, resetPasswordSentEjs, req.body, req.query.language, err.code);
+			_render(res, selfForgotPasswordEjs, req.body, language, USER_NOT_FOUND);
 		} else {
 			logger.error(err);
 			res.status(500);
@@ -300,13 +313,119 @@ app.post(RESEND_NOTIFICATION, bodyParser.urlencoded({extended: false}), function
 });
 
 app.get(ACCOUNT_CONFIRMED_PAGE, function (req, res) {
-	let options = {
-		error: req.query.error,
-		errorCode: req.query['error_code'],
-		errorDescription: req.query['error_description'],
-		uuid: req.query.uuid
-	};
-	_render(res, accountConfirmedEjs, options, req.query.language);
+	let secretKey = req.query.secretKey;
+	let language = req.query.language;
+	
+	selfServiceManager.getSignUpConfirmationResult(secretKey).then(function (result) {
+		let options = {
+			errorStatusCode: '',
+			errorDescription: '',
+			uuid: result && result.uuid
+		};
+		if (result && result.success) {
+			logger.debug('sign up result - success');
+			_render(res, accountConfirmedEjs, options, language);
+		} else {
+			if (result.error.code === 'GONE') {
+				logger.debug('sign up result - failure: ' + result.error.description);
+				options.errorStatusCode = 'GONE';
+				options.errorDescription = result.error.description;
+				_render(res, accountConfirmedEjs, options, language);
+			} else if (result.error.code === 'NOT_FOUND') {
+				logger.debug('sign up result - failure: ' + result.error.description);
+				options.errorStatusCode = 'NOT_FOUND';
+				options.errorDescription = result.error.description;
+				_render(res, accountConfirmedEjs, options, language);
+			} else {
+				logger.error('unexpected sign up result ' + result);
+				res.status(500);
+				res.send('Something went wrong');
+			}
+		}
+	}).catch(function (err) {
+		logger.error(err);
+		res.status(500);
+		res.send('Something went wrong');
+	});
+});
+
+app.get(RESET_PASSWORD_PAGE, function (req, res) {
+	let secretKey = req.query.secretKey;
+	let language = req.query.language;
+	
+	selfServiceManager.getForgotPasswordConfirmationResult(secretKey).then(function (result) {
+		if (result && result.success) {
+			//generate one time code and pass it to the reset password form,
+			// here we do that in memory but it better to use DB like Redis to do that and store it for temporary time.
+			let oneTimeCode = base64url.encode(crypto.randomBytes(24));
+			let uuid = result.uuid;
+			resetPasswordCodesMap.set(oneTimeCode, {uuid: uuid ,tenantId: tenantId});
+			logger.debug('rendering ' + resetPasswordFormEjs);
+			_render(res, resetPasswordFormEjs, {uuid: uuid, code: oneTimeCode}, language);
+		} else {
+			if (result.error.code === 'NOT_FOUND') {
+				logger.debug('forgot password result - failure: ' + result.error.description);
+				_render(res, resetPasswordExpiredEjs, {}, language);
+			} else {
+				logger.error('unexpected forgot password result ' + result);
+				res.status(500);
+				res.send('Something went wrong');
+			}
+		}
+	}).catch(function (err) {
+		logger.error(err);
+		res.status(500);
+		res.send('Something went wrong');
+	});
+});
+
+app.post(RESET_PASSWORD_SUBMIT, bodyParser.urlencoded({extended: false}), function(req, res) {
+	let uuid = req.body && req.body.uuid;
+	let code = req.body && req.body.code;
+	let newPassword = req.body['new_password'];
+	let confirmNewPassword = req.body['confirmed_new_password'];
+	let language = req.query.language || 'en';
+	
+	if (newPassword !== confirmNewPassword) {
+		logger.debug('rendering reset password with error: password not the same');
+		_render(res, resetPasswordFormEjs, {}, language, "passwords_mismatch");
+	} else {
+		//validate the the passed code was generate by us
+		let codeObject = resetPasswordCodesMap.get(code);
+		if (codeObject) {
+			resetPasswordCodesMap.delete(code);
+			if (uuid === codeObject.uuid && tenantId === codeObject.tenantId) {
+				//update the password and render the success page
+				selfServiceManager.setUserNewPassword(uuid, newPassword).then(function (user) {
+					logger.debug('successfully update user password');
+					let email = user.emails[0].value;
+					_render(res, resetPasswordSuccess, {email: email}, language);
+				}).catch(function (err) {
+					if (err.code) {
+						logger.debug("error code:" + err.code + " ,bad reset password input: " + err.message);
+						_render(res, resetPasswordFormEjs, {}, language, err.code);
+					} else {
+						logger.error('Error while trying to save user new password: ' + err.message);
+						res.status(500);
+						res.send('Something went wrong');
+					}
+				});
+				
+			} else {
+				logger.error('The stored code object does not match the passed parameters');
+				res.status(500);
+				res.send('Something went wrong');
+			}
+			
+		} else {
+			logger.error('The supplied code was not found in the resetPasswordCodesMap');
+			res.status(500);
+			res.send('Something went wrong');
+		}
+	}
+	
+
+	
 });
 
 var port = process.env.PORT || 1234;
