51848669de0014317a17d4054669ea8cefe044fd
rotembr <rotembr@il.ibm.com>
2018-02-27 15:55:22 +0200

    added  "use strict"

--- test/self-service-manager-test.js
+++ test/self-service-manager-test.js
@@ -10,7 +10,7 @@
  See the License for the specific language governing permissions and
  limitations under the License.
  */
-
+"use strict";
 const chai = require("chai");
 const assert = chai.assert;
 const rewire = require("rewire");
